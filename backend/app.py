6a4a3d9994725040bfe999a826d7cb7cb4e1646b
Jerzy Pawlikowski <jurekpawlikowski@gmail.com>
2018-09-26 19:19:47 +0200

    add zipkin

--- backend/app.py
+++ backend/app.py
@@ -3,17 +3,34 @@
 from asyncio import sleep
 from random import random
 
+import aiozipkin as az
 from aiohttp import web
 
 
 async def get(request):
-    task_time = 2 * random()
+    task_time = random()
     await sleep(task_time)
     return web.json_response(data={"task_time": task_time})
 
-app = web.Application()
-app.add_routes([
-    web.get("/", get)
-])
 
-web.run_app(app, port=8081)
+async def make_app(host, port):
+    app = web.Application()
+    app.add_routes([web.get("/", get)])
+
+    endpoint = az.create_endpoint("backend", ipv4=host, port=port)
+    zipkin_address = "http://zipkin:9411/api/v1/spans"
+    tracer = await az.create(zipkin_address, endpoint, sample_rate=1.0)
+    az.setup(app, tracer)
+    return app
+
+
+def run():
+    host = "backend"
+    port = 8081
+    app = make_app(host, port)
+    web.run_app(app, port=port)
+
+
+if __name__ == "__main__":
+    run()
+
--- frontend/app.py
+++ frontend/app.py
@@ -1,21 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import aiozipkin as az
 from aiohttp import ClientSession, web
 
 
-async def fetch(session, url):
-    async with session.get(url) as response:
+async def fetch(session, url, ctx):
+    async with session.get(url, trace_request_ctx=ctx) as response:
         return await response.json()
 
 
 async def get(request):
-    async with ClientSession() as session:
-        response = await fetch(session, "http://backend:8081/")
+    session = request.app["session"]
+    span = az.request_span(request)
+    ctx = {'span_context': span.context}
+    response = await fetch(session, "http://backend:8081/", ctx)
     task_time = response["task_time"]
     return web.Response(text=f"The task was completed in: {task_time}")
 
 
-app = web.Application()
-app.add_routes([web.get("/", get)])
+async def make_app(host, port):
+    app = web.Application()
+    app.add_routes([web.get("/", get)])
 
-web.run_app(app, port=8080)
+    endpoint = az.create_endpoint("frontend", ipv4=host, port=port)
+    zipkin_address = "http://zipkin:9411/api/v1/spans"
+    tracer = await az.create(zipkin_address, endpoint, sample_rate=1.0)
+
+    trace_config = az.make_trace_config(tracer)
+
+    session = ClientSession(trace_configs=[trace_config])
+    app["session"] = session
+
+    async def close_session(app):
+        await app["session"].close()
+
+    app.on_cleanup.append(close_session)
+    az.setup(app, tracer)
+    return app
+
+
+def run():
+    host = "frontend"
+    port = 8080
+    app = make_app(host, port)
+    web.run_app(app, port=port)
+
+
+if __name__ == "__main__":
+    run()
